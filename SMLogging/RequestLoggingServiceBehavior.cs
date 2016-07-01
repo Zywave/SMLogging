5416799020fe78f49e27a536a8c2cbce22ff34ae
John Cruikshank <john.cruikshank@zywave.com>
2016-07-01 15:36:46 -0500

    feat: behavior attribute

--- SMLogging/RequestLoggingServiceBehavior.cs
+++ SMLogging/RequestLoggingServiceBehavior.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.ObjectModel;
 using System.ServiceModel;
 using System.ServiceModel.Channels;
 using System.ServiceModel.Description;
@@ -6,12 +7,29 @@ using System.ServiceModel.Dispatcher;
 
 namespace SMLogging
 {
-    public class RequestLoggingServiceBehavior : IServiceBehavior
+    /// <summary>
+    /// Represents a request logging service behavior that adds a message inspector to log service requests.
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Class)]
+    public class RequestLoggingServiceBehavior : Attribute, IServiceBehavior
     {
+        /// <summary>
+        /// Provides the ability to pass custom data to binding elements to support the contract implementation.
+        /// </summary>
+        /// <param name="serviceDescription">The service description of the service.</param>
+        /// <param name="serviceHostBase">The host of the service.</param>
+        /// <param name="endpoints">The service endpoints.</param>
+        /// <param name="bindingParameters">Custom objects to which binding elements have access.</param>
         public void AddBindingParameters(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase, Collection<ServiceEndpoint> endpoints, BindingParameterCollection bindingParameters)
         {
         }
 
+        /// <summary>
+        /// Provides the ability to change run-time property values or insert custom extension objects such as error handlers, message or parameter 
+        /// interceptors, security extensions, and other custom extension objects.
+        /// </summary>
+        /// <param name="serviceDescription">The service description.</param>
+        /// <param name="serviceHostBase">The host that is currently being built.</param>
         public void ApplyDispatchBehavior(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase)
         {
             for (var i = 0; i < serviceHostBase.ChannelDispatchers.Count; i++)
@@ -28,6 +46,11 @@ namespace SMLogging
             }
         }
 
+        /// <summary>
+        /// Provides the ability to inspect the service host and the service description to confirm that the service can run successfully.
+        /// </summary>
+        /// <param name="serviceDescription">The service description.</param>
+        /// <param name="serviceHostBase">The service host that is currently being constructed.</param>
         public void Validate(ServiceDescription serviceDescription, ServiceHostBase serviceHostBase)
         {
         }
